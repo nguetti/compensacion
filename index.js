062c10e1a3012b018dcfbfed1183538f28a88c2a
Nicanor <nicanorguetti@gmail.com>
2023-02-17 22:07:29 -0300

    cambiando express.static a public

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.urlencoded({ extended : false })) ;
 
 // ************ Route System require and use() ************
 const mainRouter = require('./routers/mainrouter')
-const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath)) ;
+const public = path.resolve(__dirname, './public');
+app.use( express.static(public)) ;
 
 // ************ Template Engine - (don't touch) ************
 app.set('views', path.resolve(__dirname, 'views')) ;
@@ -23,5 +23,6 @@ app.set('view engine', 'ejs') ;
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{console.log("Servidor corriendo en puerto 3000")} )   
 app.use('/', mainRouter);
+
 
 
